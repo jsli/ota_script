f09f8fc46814eb1866304287c0a3ccaadb0fc44a
Jinsong Li <jsli@marvell.com>
2013-11-08 14:13:05 +0800

    Sync temp

--- sync/bak/sync_daemon.sh
+++ sync/bak/sync_daemon.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-USER_NAME=srv-release
-PASSWORD="ZPu9uPt2)n*tLV(~DRQ9"
-
-REMOTE_SERVER=//sh2-filer02/Release
 LOCAL_ROOT=/home/manson/OTA
 LOCAL_MIRROR=$LOCAL_ROOT/release/CP/CP_SERVER_MIRROR
-LOCAL_RELEASE=$LOCAL_ROOT/LOCAL_RELEASE
-
+LOCAL_RELEASE=$LOCAL_ROOT/release/CP/CP_SYNC
 EXCLUDE_FILE=exclude.list
-
-#mount remote server
-sudo umount $REMOTE_SERVER
-sudo mount -t smbfs -o username=$USER_NAME,password=$PASSWORD $REMOTE_SERVER $LOCAL_MIRROR
-
+    rsync -q -p -o -g --delete -avzh --include="*.bin" --exclude-from=$EXCLUDE_FILE $LOCAL_MIRROR/HL/HLTD $LOCAL_RELEASE/HL
 #rsync cp images
 #while true
 #do
